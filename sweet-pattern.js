d6506785f1b5af0c47a6bc10e6c76652231a5840
germtb <germtf@gmail.com>
2016-01-28 19:57:34 +0000

    Add support for funcitons with no parameters

--- sweet-pattern.js
+++ sweet-pattern.js
@@ -1,23 +1,38 @@
 macro def {
-  rule { $f:ident $rest ... ;  } => {
+
+  // No parameter functions
+  rule { $f:ident() => $exp:expr ; } => {
     const $f = function() {
-      const arg0 = arguments[0];
-      def $rest ... ;
+      return $exp;
     }
   }
 
-  rule { ($x:lit) => $exp:expr , $rest ... ; } => {
+  // One parameter functions
+  rule { $f:ident($x:lit) $rest ... ;  } => {
+    const $f = function() {
+      const arg0 = arguments[0];
+      def ($x) $rest ... ;
+    }
+  }
+
+  rule { $f:ident($x:ident) => $exp:expr ; } => {
+    const $f = function($x) {
+      return $exp;
+    }
+  }
+
+  rule { ($x:lit) => $exp:expr $rest ... ; } => {
     if (arg0 === $x) {
       return $exp
     }
-    def , $rest ... ;
+    def $rest ... ;
   }
 
-  rule {, $f:ident($x:lit) => $exp:expr , $rest ... ; } => {
+  rule {, $f:ident($x:lit) => $exp:expr $rest ... ; } => {
     else if (arg0 === $x) {
       return $exp
     }
-    def , $rest ... ;
+    def $rest ... ;
   }
 
   rule {, $f:ident($x) => $exp:expr ; } => {
@@ -25,7 +40,12 @@ macro def {
       return ($x => $exp)(arg0);
     }
   }
+
 }
+
+def f0() => "Hello sweet";
+
+def f1(x) => x * x;
 
 def fib(1) => 1,
     fib(2) => 1,
