713adf9469eaa03eb99ac82bf3342888eaa07a5b
germtb <germtf@gmail.com>
2016-01-28 19:01:14 +0000

    Add support for un argument functions

---
+++ sweet-pattern.js
@@ -0,0 +1,32 @@
+macro def {
+  rule { $f:ident $rest ... ;  } => {
+    const $f = function() {
+      arg0 = arguments[0];
+      def $rest ... ;
+    }
+  }
+
+  rule { ($x:lit) => $exp:expr , $rest ... ; } => {
+    if (arg0 === $x) {
+      return $exp
+    }
+    def , $rest ... ;
+  }
+
+  rule {, $f:ident($x:lit) => $exp:expr , $rest ... ; } => {
+    else if (arg0 === $x) {
+      return $exp
+    }
+    def , $rest ... ;
+  }
+
+  rule {, $f:ident($x) => $exp:expr ; } => {
+    else {
+      return ($x => $exp)(arg0);
+    }
+  }
+}
+
+def fib(1) => 1,
+    fib(2) => 1,
+    fib(n) => fib(n - 1) + fib(n - 2);
